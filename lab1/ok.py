7655be93a5d4f2eebea166312cb539e2d04e7704
OlexandraKovach2 <105677310+OlexandraKovach2@users.noreply.github.com>
2023-03-15 21:26:04 +0300

    lab 1 update ♥♥♥♥

--- lab1/ok.py
+++ lab1/ok.py
@@ -1,13 +1,27 @@
-import requests
+from requests import get
 from bs4 import BeautifulSoup
 
-URL = "https://www.uzhnu.edu.ua/uk/cat/faculty"
+URL = "https://devdocs.io/angular~12/"
 HEADERS = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 }
-page = requests.get(URL, headers=HEADERS)
-soup = BeautifulSoup(page.text)
-
-with open('ok.txt', 'w') as f:
+page = get(URL, headers=HEADERS)
+soup = BeautifulSoup(page.text, 'lxml')
+links = []
+with open('./ok.txt', 'w') as file:
     for link in soup.find_all('a'):
-        f.write(f"{link.get('href')}\n")
+        if link.get('href') is not None:
+            if link.get('href') not in links:
+                if 'https' in link.get('href'): 
+                    file.write("href: {0}\n".format(link.get('href')))
+                    links.append(link.get('href'))
+    for link in links:
+        URL = link
+        page = get(URL, headers=HEADERS)
+        soup = BeautifulSoup(page.text, 'lxml')
+        for newlink in soup.find_all('a'):
+            if newlink.get('href') is not None:
+                if newlink.get('href') not in links:
+                    if 'https' in newlink.get('href'): 
+                        file.write("href: {0}\n".format(newlink.get('href')))
+                        links.append(newlink.get('href'))
